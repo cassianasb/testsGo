9f79a2617ac905061e8b0dad9d3b943809ccdc51
cassianasb <kassi.ice@gmail.com>
2018-09-02 12:00:23 -0300

    Fist code from course

---
+++ curso-go/3-fundamentos/primeiro/primeiro.go
@@ -0,0 +1,16 @@
+//Programas executam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import (
+	"fmt"
+)
+
+//A porta de entrada do programa em Go é a função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
