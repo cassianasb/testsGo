5e7e2320d2598238c36e82d134129c05e9c76f05
cassianasb <kassi.ice@gmail.com>
2018-11-04 14:30:22 -0200

    functions 101

---
+++ curso-go/3-fundamentos/funcoes/funcoes.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func somar(x int, y int) int {
+	return x + y
+}
+
+func imprimir(valor int) {
+	fmt.Println(valor)
+}
---
+++ curso-go/3-fundamentos/funcoes/main.go
@@ -0,0 +1,7 @@
+package main
+
+//dessa forma executa apenas pelo terminal por meio do comando go run *.go dentro da pasta do arquivo com a funcao main
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
