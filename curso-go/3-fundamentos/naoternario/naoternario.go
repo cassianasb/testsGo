ea7540fee48396325008c7c7113428e6867f15a4
cassianasb <kassi.ice@gmail.com>
2018-12-02 16:42:33 -0200

    não tem ternario

---
+++ curso-go/3-fundamentos/naoternario/naoternario.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Não tem operador ternario
+func obterResultado(nota float64) string {
+	if nota >= 6 {
+		return "Aprovado"
+	}
+	return "Reprovado"
+	// return nota>=6 ? "Aprovado" : "Reprovado"
+}
+
+func main() {
+	fmt.Println(obterResultado(6.3))
+}
