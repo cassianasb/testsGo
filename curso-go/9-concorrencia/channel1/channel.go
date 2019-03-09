83958970ba874833138fe8707941731ec6d370d7
cassianasb <kassi.ice@gmail.com>
2019-03-09 10:51:30 -0300

    Canal 2

--- curso-go/9-concorrencia/channel1/channel.go
+++ curso-go/9-concorrencia/channel1/channel.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	ch := make(chan int, 1) //canal com valores inteiros com 1 buffer
-
+	//Canal é um ponto de sincronismo.
 	//Enviar informação pelo canal
 	ch <- 1 //Enviando para o canal o valor 1
 
