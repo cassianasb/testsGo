977e352e844c8502cca1782cef3d9b9229c2e1e0
cassianasb <kassi.ice@gmail.com>
2019-03-09 09:12:34 -0300

    Canal

---
+++ curso-go/9-concorrencia/channel1/channel.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 1) //canal com valores inteiros com 1 buffer
+
+	//Enviar informação pelo canal
+	ch <- 1 //Enviando para o canal o valor 1
+
+	//Receber informação pelo canal
+	<-ch //Recebendo o valor do canal (não é obrigatório uma variavel para recebimento)
+
+	ch <- 2
+	fmt.Println(<-ch)
+}
